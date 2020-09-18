f3987d7ac0f672145c25e0cc0978f22addf8480f
gsg0222 <gsg0222.1977@gmail.com>
2020-09-18 09:47:00 +0900

    first commit

---
+++ src/main/java/blog/tsuchiya/todo/ToDoSampleApplication.java
@@ -0,0 +1,13 @@
+package blog.tsuchiya.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToDoSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToDoSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/blog/tsuchiya/todo/ToDoSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.tsuchiya.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
